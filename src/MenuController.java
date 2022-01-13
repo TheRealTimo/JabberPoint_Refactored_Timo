bf8f3e62827a4dac76369f5f18b10f2375fcc9f5
Timo <31155789+TheRealTimo@users.noreply.github.com>
2022-01-13 19:51:08 +0100

    Removed AboutBox class
    Removed about box and moved functionality to MenuController, where the About Menu resides

--- src/MenuController.java
+++ src/MenuController.java
@@ -110,7 +110,17 @@ public class MenuController extends MenuBar {
         helpMenu.add(menuItem = mkMenuItem(ABOUT));
         menuItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent) {
-                AboutBox.show(parent);
+                JOptionPane.showMessageDialog(parent,
+                        """
+                                JabberPoint is a primitive slide-show program in Java(tm). It
+                                is freely copyable as long as you keep this notice and
+                                the splash screen intact.
+                                Copyright (c) 1995-1997 by Ian F. Darwin, ian@darwinsys.com.
+                                Adapted by Gert Florijn (version 1.1) and Sylvia Stuurman (version 1.2 and higher) for the OpenUniversity of the Netherlands, 2002 -- now.
+                                Author's version available from http://www.darwinsys.com/""",
+                        "About JabberPoint",
+                        JOptionPane.INFORMATION_MESSAGE
+                );
             }
         });
         setHelpMenu(helpMenu);        //Needed for portability (Motif, etc.).
